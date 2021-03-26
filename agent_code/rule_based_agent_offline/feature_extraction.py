29f6f85fa828fd4d8e135dcc152e9a098a89c7c6
Aisolon <b.mumme@stud.uni-heidelberg>
2021-03-26 20:58:55 +0100

    Designing features

--- agent_code/rule_based_agent_offline/feature_extraction.py
+++ agent_code/rule_based_agent_offline/feature_extraction.py
@@ -4,12 +4,15 @@ import pickle
 featurepickle = open("./featureset.pt", "rb")
 labelpickle = open("./labelset.pt", "rb")
 
-features = pickle.load(featurepickle)
+gamestates = pickle.load(featurepickle)
 labels = pickle.load(labelpickle)
 
-for idx, game_state in enumerate(features):
+all_features = []
 
-	# print(idx, game_state)
+for idx, game_state in enumerate(gamestates):
+
+	features = []
+	
 	field = game_state['field']
 	walls = (game_state['field'] == -1).astype(int)
 	crates = (game_state['field'] == 1).astype(int)
@@ -21,19 +24,77 @@ for idx, game_state in enumerate(features):
 
 	# print(walls)
 
+	# Current Step Number
+	step_number = game_state['step']
+	features.append(step_number)
+
+	# Sum of crates in radius 1 around agent
 
 
+	# Sum of crates in radius 2 around agent
 
 
+	# Agent in explosion zone (bool)
 
 
+	# Agent straight distance from next bomb (4 if bomb further away than 4)
 
 
+	# L1-distance to next agent
 
 
+	# Coins in radius 3
 
 
+	# L1-distance to next coin
 
+
+	# Is bomb setting possible?
+	bomb_possible = game_state['self'][2]
+	features.append(bomb_possible)
+
+	# Agent score
+	agent_score = game_state['self'][1]
+	features.append(agent_score)
+
+	# Other agents scores
+	for agent in game_state['others']:
+		features.append(agent[1]) 
+
+	# Lowest countdown for bomb that's less than 4 spaces away in straight line
+
+
+	# Sum of all crates
+
+
+	# Crates in radius 1 around next coin
+
+
+	# Crates in radius 1 around next agent
+
+
+	# Space free of crates in each direction
+
+
+	# Crates that explode if bomb is placed at current location
+
+
+	# Agents that explode if bomb is placed at current location (and agent doesn't move)
+
+
+	# Chance of survival if bomb places here (bool)
+
+
+	# Relative coordinates of next coin
+
+
+	# Relative coordinates of next agent
+
+
+	# Agent is at left/right border (bool)
+
+
+	# Agent is at top/bottom border (bool)
 
 
 
